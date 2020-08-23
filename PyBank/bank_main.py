3c154c97e4dc82214b55a93e4e362d7e9004a555
David <davidmrtnz20@gmail.com>
2020-08-22 20:50:07 -0500

    fixed average change, great inc, and great dec

--- PyBank/bank_main.py
+++ PyBank/bank_main.py
@@ -4,13 +4,13 @@ import csv
 budget_data = os.path.join("Resources", "budget_data.csv")
 
 net = []
-avrg = []
-great_increase = []
-great_decrease = []
+month_to_month_changes = []
+dates = []
 
 total = 0
 net_total = 0
-average_change = 0
+previous_change = 0
+total_change = 0
 
 with open(budget_data) as csv_file:
 
@@ -24,31 +24,40 @@ with open(budget_data) as csv_file:
         net.append(row[1])
         net_total = net_total + float(row[1])
 
-        avrg.append(row[1])
-        average_change = (average_change + float(row[1])) / total
+        change = int(row[1])
+        monthly_change = change - previous_change
 
-        great_increase.append(row[1])
-        great_decrease.append(row[1])
+        dates.append(row[0])
+        month_to_month_changes.append(monthly_change)
+
+        total_change = total_change + monthly_change
+        previous_change = change
+        average_change = (total_change/total)
+
+        great_increase = max(month_to_month_changes)
+        increase_date = dates[month_to_month_changes.index(great_increase)]
+        great_decrease = min(month_to_month_changes)
+        decrease_date = dates[month_to_month_changes.index(great_decrease)]
 
 print("")
 print("Financial Analysis")
 print("----------------------------")       
 print(f"Total Months: {total}")
-print(f"Net Total: $ {net_total}")
-print(f"Average Change: $ {average_change}")
-print(f"Greatest Increase in Profits: $ {max(great_increase)}")
-print(f"Greatest Decrease in Profits: $ {min(great_decrease)}")
+print(f"Total: $ {net_total}")
+print(f"Average Change: $ {round(average_change,2)}")
+print(f"Greatest Increase in Profits: $ {increase_date} {great_increase}")
+print(f"Greatest Decrease in Profits: $ {decrease_date} {great_decrease}")
 print("----------------------------")
 
 import sys
-sys.stdout = open("Financial Analysis", "w")
+sys.stdout = open("Analysis/Financial Analysis", "w")
 print("Financial Analysis")
 print("----------------------------")       
 print(f"Total Months: {total}")
-print(f"Net Total: $ {net_total}")
-print(f"Average Change: $ {average_change}")
-print(f"Greatest Increase in Profits: $ {max(great_increase)}")
-print(f"Greatest Decrease in Profits: $ {min(great_decrease)}")
+print(f"Total: $ {net_total}")
+print(f"Average Change: $ {round(average_change,2)}")
+print(f"Greatest Increase in Profits: $ {increase_date} {great_increase}")
+print(f"Greatest Decrease in Profits: $ {decrease_date} {great_decrease}")
 print("----------------------------")
 
 
